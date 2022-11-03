2d41f2f385cf8f999b3f50d76175c5c9a5b5b029
Bappy dase <bappydas.bd51@gmail.com>
2022-11-03 13:20:50 +0600

    review slider

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,84 +1,78 @@
 import React from "react";
 import bannerBG from "../../assets/311634251_2178888665651407_1413025571303879858_n.jpg";
-
+import './Reviews'
 import { HiOutlineSearch } from "react-icons/hi";
 import Review from "./Review/Review";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-// import required modules
 import { Navigation, Pagination,Autoplay } from "swiper";
 
 const Reviews = () => {
-//   let navigation = useNavigate();
 
 	return (
 	  <section className="bg-coal text-gray-200 body-font px-24">
-	  	{/* <div className="relative max-h-full md:pb-0 min-h-screen bg-cover" style={{ backgroundImage: `url(${bannerBG})` }}> */}
-      		<div className="container mx-auto grid md:grid-cols-1 xs:grid-cols-1 py-0 absuolute top-1/2 left-1/2  translate-1/2">
-				<div className="grid items-center text-center my-20"> 
-          			<div className="text-1xl lg:text-2xl xl:text-3xl font-bold mt-2 text-white">
-						WHAT PEOPLE SAY					  
-				  	</div>
+      	<div className="container mx-auto grid md:grid-cols-1 xs:grid-cols-1 py-0 absuolute top-1/2 left-1/2  translate-1/2">
+			<div className="grid items-center text-center my-20">
+        		<div className="text-1xl lg:text-2xl xl:text-3xl font-bold mt-2 text-white">
+					WHAT PEOPLE SAY
 			  	</div>
-			  	<div className="text-center my-20">
-					  <Swiper
-          				  slidesPerView={1}
-          				  spaceBetween={30}
-          				  centeredSlides={true}
-          				  autoplay={{
-          				    delay: 2500,
-          				    disableOnInteraction: false,
-          				  }}
-          				  pagination={{
-          				    clickable: true,
-          				  }}
-          				  navigation={true}
-          				  modules={[Autoplay, Pagination, Navigation]}
-          				  className="mySwiper"
-          				  breakpoints={{
-          				    "@0.00": {
-          				      slidesPerView: 1,
-          				    },
-          				    "@1.50": {
-          				      slidesPerView: 1,
-          				    },
-          				    "@1.75": {
-          				      slidesPerView: 1,
-          				    },
-          				  }}
-          				>
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>          
-          				  <SwiperSlide className="pb-20">
-          				    <Review/>
-          				  </SwiperSlide>          
-          				</Swiper>
-				</div>
-      		</div>
-    	{/* </div> */}
+		  	</div>
+		  	<div className="text-center my-20">
+				  <Swiper
+        			  slidesPerView={1}
+        			  spaceBetween={30}
+        			  centeredSlides={true}
+        			  autoplay={{
+        			    delay: 2500,
+        			    disableOnInteraction: false,
+        			  }}
+        			  pagination={{
+        			    clickable: true,
+        			  }}
+        			  navigation={true}
+        			  modules={[Autoplay, Pagination, Navigation]}
+        			  className="mySwiper"
+        			  breakpoints={{
+        			    "@0.00": {
+        			      slidesPerView: 1,
+        			    },
+        			    "@1.50": {
+        			      slidesPerView: 1,
+        			    },
+        			    "@1.75": {
+        			      slidesPerView: 1,
+        			    },
+        			  }}
+        			>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			  <SwiperSlide className="pb-24">
+        			    <Review/>
+        			  </SwiperSlide>
+        			</Swiper>
+			</div>
+      	</div>
 	</section>
 )};
 
