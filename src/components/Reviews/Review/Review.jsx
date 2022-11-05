eaf871e1fa91c87564babae67f81cc548d0b4acc
Bappy dase <bappydas.bd51@gmail.com>
2022-11-05 18:04:57 +0600

    review section complete

--- src/components/Reviews/Review/Review.jsx
+++ src/components/Reviews/Review/Review.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import profileImage from '../../../assets/3135715.png'
 
 const Review = () => {
     return (
@@ -7,7 +8,11 @@ const Review = () => {
                 <h1 class="text-lg md:text-1xl font-semibold text-white xl:text-3xl lg:w-[500px] leading-relaxed italic tracking-normal">
 					“Photography is an art. And when you are part of
 					art, this is bliss.”
-				</h1>
+                </h1>
+                <div className='flex flex-col justify-center items-center mt-10'>
+                    <img className='h-16 w-16' src={profileImage} alt="" srcset="" />
+                    <p className='text-sm font-semibold mt-5'>Abul Monshi</p>
+                </div>
             </div>
         </section>
     );
